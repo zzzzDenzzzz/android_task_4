b4f9d74fc3c8b3c5819c9b70592416363edf4b92
zzzzDenzzzz <12den24@gmail.com>
2024-07-29 20:58:54 +0300

    first commit

---
+++ app/src/main/java/com/example/app_4/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.app_4;
+
+import static com.example.app_4.StringConst.*;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.app_4.databinding.ActivityMainBinding;
+
+import java.time.LocalDateTime;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        binding.showAlertDialogButton.setOnClickListener(v -> showAlertDialog());
+        binding.exitButton.setOnClickListener(v -> finish());
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(CHOOSE_AN_ACTION)
+                .setMessage(SELECT_ONE_OF_THE_ACTIONS_BELOW)
+                .setPositiveButton(SET_TIME, (dialog, which) -> showTimePickerDialog())
+                .setNegativeButton(SET_DATE, (dialog, which) -> showDatePickerDialog())
+                .setNeutralButton(CANCEL, (dialog, which) -> dialog.dismiss())
+                .create()
+                .show();
+    }
+
+    private void showTimePickerDialog() {
+
+        @SuppressLint("SetTextI18n")
+        TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this,
+                (view, hourOfDay, minuteOfHour) -> {
+                    @SuppressLint("DefaultLocale")
+                    String selectedTime = String.format("%02d:%02d", hourOfDay, minuteOfHour);
+                    binding.timeTextView.setText(SELECTED_TIME + selectedTime);
+                },
+                LocalDateTime.now().getHour(),
+                LocalDateTime.now().getMinute(),
+                true);
+        timePickerDialog.show();
+    }
+
+    private void showDatePickerDialog() {
+
+        @SuppressLint("SetTextI18n")
+        DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,
+                (view, year1, month1, dayOfMonth) -> {
+                    @SuppressLint("DefaultLocale")
+                    String selectedDate = String.format("%02d-%02d-%04d", dayOfMonth, month1 + 1, year1);
+                    binding.dateTextView.setText(SELECTED_DATE + selectedDate);
+                },
+                LocalDateTime.now().getYear(),
+                LocalDateTime.now().getMonthValue()-1,
+                LocalDateTime.now().getDayOfMonth());
+        datePickerDialog.show();
+    }
+}
---
+++ app/src/main/java/com/example/app_4/StringConst.java
@@ -0,0 +1,12 @@
+package com.example.app_4;
+
+public class StringConst {
+
+    public static final String CHOOSE_AN_ACTION = "Выберите действие";
+    public static final String SELECT_ONE_OF_THE_ACTIONS_BELOW = "Выберите одно из действий ниже";
+    public static final String SET_TIME = "Установить время";
+    public static final String SET_DATE = "Установить дату";
+    public static final String CANCEL = "Отмена";
+    public static final String SELECTED_TIME = "Выбранное время: ";
+    public static final String SELECTED_DATE = "Выбранная дата: ";
+}
